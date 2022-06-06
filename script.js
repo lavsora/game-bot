126430b977c603a70bf10a7bd9fa1684c30e9198
lavsora <ya.karpich@gmail.com>
2022-06-06 17:46:43 +0300

    update bot

--- script.js
+++ script.js
@@ -1,48 +1,60 @@
 'use strict'
 
-let game;
-
 const gameBot = function () {
-    let gameNumber = Math.round(Math.random()*100)
-    return function () {
-        let userNumber = prompt('Угадай число от 1 до 100');
+    let gameNumber = Math.round(Math.random() * 100)
+    let attempt = 10;
+    let questionAttempt;
+    let questionWin;
 
-        const isNumber = function(num) {
-           return !isNaN(num) && /\s/g.test(num) && isFinite(num)
-        }
+    const isNumber = function (num) {
+        return !isNaN(num) && !/\s/g.test(num) && isFinite(num)
+    }
 
-        if (userNumber === null) {
-            alert('Игра окончена')
+    const checkQuestionAttempt = function () {
+        if (questionAttempt === true) {
+            gameBot();
+        } else {
+            alert('До свидания. Приходите играть еще!')
             return
+        }
+    }
 
-        } else if (isNumber(userNumber)) {
+    const checkQuestionWin = function () {
+        if (questionWin === true) {
+            gameBot();
+        } else {
+            alert('До свидания. \nПриходите играть еще!')
+            return
+        }
+    }
+
+    const game = function () {
+        const userNumber = prompt('Угадайте число от 1 до 100');
+
+        if (attempt === 0) {
+            questionAttempt = confirm('Попытки закончились. \nХотите сыграть еще?')
+            checkQuestionAttempt();
+        } else if (userNumber === null) {
+            alert('Игра окончена.')
+            return
+        } else if (!isNumber(userNumber)) {
             alert('Введи число!')
             game();
-        }
-
-        if (+userNumber === gameNumber) {
-            alert('Поздравляю, Вы угадали!!!');
+        } else if (+userNumber === gameNumber) {
+            questionWin = confirm('Поздравляю, Вы угадали!!! \nЗагаданное число: ' + gameNumber + ' \nХотели бы сыграть еще?');
+            checkQuestionWin();
         } else if (+userNumber > gameNumber) {
-            alert('Загаданное число меньше');
+            attempt--;
+            alert('Загаданное число меньше. \nОсталось попыток: ' + attempt);
             game();
         } else if (+userNumber < gameNumber) {
-            alert('Загаданное число больше');
+            attempt--;
+            alert('Загаданное число больше. \nОсталось попыток: ' + attempt);
             game();
         }
     }
+
+    game();
 }
 
-const goGame = function () {
-    let question = confirm('Хочешь поиграть в игру?')
-
-    if (question === true) {
-        game();
-    } else {
-        alert('Не задерживайте очередь')
-        return
-    }
-}
-
-game = gameBot();
-
-goGame();
+gameBot();
