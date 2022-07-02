111c0955db4bd239960bb72e4f67222ac35b250a
lavsora <ya.karpich@gmail.com>
2022-07-03 02:16:05 +0300

    gamebot with DOM

--- script.js
+++ script.js
@@ -1,55 +1,107 @@
 'use strict'
 
-const gameBot = () => {
+const text = document.querySelector('h1');
+const form = document.querySelector('#prompt');
+const input = document.querySelector('input');
+const btnStart = document.querySelector('#btn-start');
+const btnTrueFalse = document.querySelectorAll('.btn-true-false')
+
+let attempt = 10;
+
+const printMessage = (str) => {
+    text.textContent = str;
+}
+
+input.addEventListener('input', (e) => e.target.value = e.target.value.replace(/\D+/, ''));
+
+const game = () => {
+
     let gameNumber = Math.round(Math.random() * 100)
-    let attempt = 10;
 
-    const game = () => {
-        let userNumber = prompt('Угадайте число от 1 до 100');
+    printMessage(`Угадайте число от 1 до 100. У вас есть ${attempt} попыток.`)
 
-        if (userNumber === null) {
-            alert('Игра окончена.')
-            return
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        
+        if(!(input.value === '')) {
+            gameBot(+input.value)
+    
+            input.value = '';
         }
+    });
 
-        if (isNaN(userNumber)) {
-            alert('Ошибка! Введите число!')
-        }
+    const gameBot = (userNumber) => {
+        if (attempt < 2) {
+            printMessage('Попытки закончились. Хотите сыграть еще?')
 
-        if (attempt === 0) {
-            let questionAttempt = confirm('Попытки закончились. \nХотите сыграть еще?')
+            form.style.display = 'none'
 
-            if (questionAttempt === true) {
-                return gameBot();
-            } else {
-                alert('До свидания. \nПриходите играть еще!')
+            btnTrueFalse[0].style.display = 'block';
+            btnTrueFalse[1].style.display = 'block';
+
+            btnTrueFalse[0].addEventListener('click', () => {
+                btnTrueFalse[0].style.display = 'none';
+                btnTrueFalse[1].style.display = 'none';
+
+                form.style.display = 'block'
+
+                gameNumber = Math.round(Math.random() * 100)
+                attempt = 10
+
+                return game()
+            });
+
+            btnTrueFalse[1].addEventListener('click', () => {
+                btnTrueFalse[0].style.display = 'none';
+                btnTrueFalse[1].style.display = 'none';
+
+                printMessage('До свидания. Приходите играть еще!')
+
+                form.style.display = 'none'
+
                 return
-            }
-
+            })
         }
 
         attempt--;
 
-        if (+userNumber === gameNumber) {
-            let questionWin = confirm('Поздравляю, Вы угадали!!! \nЗагаданное число: ' + gameNumber + ' \nХотели бы сыграть еще?');
+        if (userNumber === gameNumber) {
+            printMessage(`Поздравляю, Вы угадали!!! Загаданное число: ${gameNumber} Хотели бы сыграть еще?`)
 
-            if (questionWin === true) {
-                return gameBot();
-            } else {
-                alert('До свидания. \nПриходите играть еще!')
+            form.style.display = 'none'
+
+            btnTrueFalse[0].style.display = 'block';
+            btnTrueFalse[1].style.display = 'block';
+
+            btnTrueFalse[0].addEventListener('click', () => {
+                btnTrueFalse[0].style.display = 'none';
+                btnTrueFalse[1].style.display = 'none';
+
+                form.style.display = 'block'
+
+                gameNumber = Math.round(Math.random() * 100)
+                attempt = 10
+
+                return game()
+            });
+
+            btnTrueFalse[1].addEventListener('click', () => {
+                btnTrueFalse[0].style.display = 'none';
+                btnTrueFalse[1].style.display = 'none';
+
+                printMessage('До свидания. Приходите играть еще!')
+
+                form.style.display = 'none'
+
                 return
-            }
+            })
 
-        } else if (+userNumber > gameNumber) {
-            alert('Загаданное число меньше. \nОсталось попыток: ' + attempt);
-        } else if (+userNumber < gameNumber) {
-            alert('Загаданное число больше. \nОсталось попыток: ' + attempt);
+        } else if (userNumber > gameNumber) {
+            printMessage(`Загаданное число меньше. Осталось попыток: ${attempt}`)
+        } else if (userNumber < gameNumber) {
+            printMessage(`Загаданное число больше. Осталось попыток: ${attempt}`)
         }
-
-        return game();
     }
-
-    return game();
 }
 
-gameBot();
+game()
